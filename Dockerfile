ed12cb04470d4b91e37523b38a66922fc1383eb9
Oskar Szymczyk <oskarszymczyk67@gmail.com>
2024-01-26 15:11:04 +0100

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.8-slim
 WORKDIR /app
-COPY main.py .
+COPY . ./
 ENV APP_NAME="HelloWorldPythonApp"
 ENV APP_VERSION="1.0"
+RUN pip install --no-cache-dir -r requirements.txt
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('Hello World! This a basic, containerized Hello World Python app.')
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    """Example Hello World route."""
+    name = os.environ.get("APP_NAME")
+    version = os.environ.get("APP_VERSION")
+    print(f"Hello World! App name: {name}; App version: {version}")
+    return f"Hello World!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
