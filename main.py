1415de1e9fcb5a4c038c1a75bb5f65aa2c8b7e95
Oskar Szymczyk <oskarszymczyk67@gmail.com>
2024-01-26 15:16:24 +0100

    Fixup

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     """Example Hello World route."""
-    name = os.environ.get("APP_NAME")
-    version = os.environ.get("APP_VERSION")
     return f"Hello World!"
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    name = os.environ.get("APP_NAME")
+    version = os.environ.get("APP_VERSION")
     print(f"Hello World! App name: {name}; App version: {version}")
